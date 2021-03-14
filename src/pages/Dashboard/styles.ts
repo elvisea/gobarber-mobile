ef599ace8c6cdb002507e85649ceddf3a6797de3
Elvis <elvis.e.amancio@gmail.com>
2021-03-14 01:58:58 -0300

    Page Create Appointments

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,38 +1,95 @@
 import styled from 'styled-components/native';
-import { getBottomSpace } from 'react-native-iphone-x-helper';
-import { Platform } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { FlatList } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { Provider } from '.';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
-  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
-  background: #312e38;
-`;
-export const Title = styled.Text`
-  font-size: 24px;
-  color: #f4ede8;
-  font-family: 'RobotoSlab-Medium';
-  margin: 64px 0 24px;
 `;
 
-export const BackToSignInButton = styled.TouchableOpacity`
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  background: #312e38;
-  border-top-width: 1px;
-  border-color: #232129;
-  padding: 16px 0 ${16 + getBottomSpace()}px;
-  justify-content: center;
-  align-items: center;
+export const Header = styled.View`
+  padding: 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
+  background: #28262e;
+
   flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
-export const BackToSignInText = styled.Text`
-  color: #fff;
-  font-size: 18px;
+export const HeaderTitle = styled.Text`
+  color: #f4ede8;
+  font-size: 20px;
   font-family: 'RobotoSlab-Regular';
-  margin-left: 16px;
+  line-height: 28px;
+`;
+
+export const UserName = styled.Text`
+  color: #ff9000;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+export const ProfileButton = styled.TouchableOpacity``;
+
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+`;
+
+export const ProvidersList = styled(
+  FlatList as new () => FlatList<Provider>,
+).attrs({
+  contentContainerStyle: {
+    paddingTop: 32,
+    paddingBottom: 16,
+    paddingHorizontal: 24,
+  },
+})``;
+
+export const ProvidersListTitle = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  color: #f4ede8;
+  font-size: 24px;
+  margin-bottom: 24px;
+`;
+
+export const ProviderContainer = styled(RectButton)`
+  flex-direction: row;
+  align-items: center;
+  padding: 20px;
+  margin-bottom: 16px;
+
+  background: #3e3b47;
+  border-radius: 10px;
+`;
+
+export const ProviderAvatar = styled.Image`
+  width: 72px;
+  height: 72px;
+  border-radius: 36px;
+`;
+
+export const ProviderInfo = styled.View`
+  flex: 1;
+  margin-left: 20px;
+`;
+
+export const ProviderName = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 18px;
+  color: #f4ede8;
+`;
+
+export const ProviderMeta = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 8px;
+`;
+
+export const ProviderMetaText = styled.Text`
+  margin-left: 8px;
+  color: #999591;
+  font-family: 'RobotoSlab-Regular';
 `;
